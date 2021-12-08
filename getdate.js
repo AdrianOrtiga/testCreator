0bf8af662a1ab18099ffae720ca3d3a96231167b
AdrianOrtiga <adrian.ortiga@gmail.com>
2021-12-08 17:31:11 +0800

    fisrt commit

---
+++ getdate.js
@@ -0,0 +1,6 @@
+const date = Date(Date.now())
+                    
+
+
+
+document.getElementById('Date').textContent = date.slice(4,15)
---
+++ script.js
@@ -0,0 +1,21 @@
+const questions = [
+    {text:'Write your name inside an HTML paragraph?', points:1},
+    {text:'Write your name inside an HTML Heading 1?', points:1},
+    {text:'What function can we use to display data in the console?', points:1},
+    {text:'What method can we use to pop up an alert in the browser?', points:1},
+    {text:'What function can we use to access an HTML element in javascript?', points:1},
+    {text:'What display function it should be only use for testing?', points:1},
+    {text:'What method or function can we use to print the content of the current window?', points:1},
+    {text:'How many ways do we have to declare a variable in Javascript?', points:1},
+    {text:'How do we create variable called "a" that stores the value 123?', points:1},
+    {text:'How do we create variable called "word" that stores the text "hello"?', points:1},
+]
+const question = document.getElementById('question')
+
+
+
+/*
+<span id='points' class='pnts-qstn'></span>
+<span id='number' class='numb-qstn'></span>
+<span id='text' class='qstn-text'></span>
+*/
